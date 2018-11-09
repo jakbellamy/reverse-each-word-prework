f4ff1ef016699512b9d65d129ac3bdfe33a76b36
Jakob Bellamy <jakbellamy@gmail.com>
2018-11-09 18:19:58 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(sentence)
+  array = sentence.split
+  reversed = []
+    array.each do |word|
+      reversed.push(word.reverse)
+    end
+  back_words = reversed.join(" ")
+  back_words
+end
+
+def reverse_each_word(sentence)
+  array = sentence.split
+    array.collect do |word|
+      word.reverse!
+    end
+  array.join(" ")
+end
